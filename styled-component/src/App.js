49ee4782df18487c765a1287d9f6419edfd69c1f
umingming <u_0112@naver.com>
2024-09-02 21:08:14 +0900

    feat: Define styled components

--- styled-component/src/App.js
+++ styled-component/src/App.js
@@ -1,9 +1,71 @@
+import styled, { keyframes } from "styled-components";
 
-function App() {
+const animation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+const Container = styled.div`
+  display: flex;
+`;
+const Text = styled.span`
+  color: white;
+`;
+const Box = styled.div`
+  background-color: ${({bgColor}) => bgColor || "teal"};
+  width: 100px;
+  height: 100px;
+  animation: ${animation} 1s linear;
+  span {
+    font-size: 20px;
+    &:hover {
+      color: blue;
+    }
+  };
+  span:hover {
+    color: blue;
+  }
+  ${Text} {
+    font-size: 20px;
+    &:hover {
+      color: blue;
+    }
+  }
+`;
+const Circle = styled(Box)`
+  border-radius: 50px;
+`
+const Button = styled.button`
+  background-color: tomato;
+  color: white;
+  border-radius: 15px;
+`
+const Input = styled.input.attrs({ required: true })`
+  width: 100px;
+`
+
+export default function App() {
   return (
-    <div className="App">
-    </div>
+    <Container>
+      <Input />
+      <Input />
+      <Circle>
+        <Text>Circle</Text>
+      </Circle>
+      <Text>Test</Text>
+      <Box as="header">
+        <Text as="p" >
+          Hello
+        </Text>
+        <Button>
+          Button
+        </Button>
+      </Box>
+      <Box bgColor="tomato" />
+
+    </Container>
   );
 }
-
-export default App;
