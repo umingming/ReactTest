0a611afbafa34e69e866a806555c956c0f6ca270
umingming <u_0112@naver.com>
2024-09-03 13:04:07 +0900

    feat: Update theme

--- type-script/src/App.tsx
+++ type-script/src/App.tsx
@@ -1,10 +1,19 @@
+import { styled } from "styled-components";
 import Circle from "./Circle"
+import Form from "./Form";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`
 
 function App() {
   return (
     <div>
-      <Circle bgColor="tomato" borderColor="black" />
-      <Circle />
+      <Container>
+        <Form />
+        <Circle bgColor="tomato" borderColor="black" />
+        <Circle />
+      </Container>
     </div>
   );
 }
---
+++ type-script/src/Form.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function Form() {
+    const [value, setValue] = useState("");
+    
+    function changeValue(event: React.FormEvent<HTMLInputElement>) {
+        const {
+            currentTarget: { value }
+        } = event;
+        setValue(value);
+    }
+    function submitValue(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log(event);
+    }
+
+    return (
+        <div>
+            <form onSubmit={submitValue}>
+                <input 
+                    value={value}
+                    onChange={changeValue}
+                    type="text"
+                />
+                <button>enter</button>
+            </form>
+
+        </div>
+    )
+}
