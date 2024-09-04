e81dd496df43ca0f0c527ff7e43768b471f683e4
umingming <u_0112@naver.com>
2024-09-05 02:27:57 +0900

    feat: Fetch coin list

--- coin-tracker/src/index.tsx
+++ coin-tracker/src/index.tsx
@@ -7,6 +7,7 @@ import { theme } from './styles/theme';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+// String Mode 실행시 개발 모드에서 컴포넌트가 두번 랜더링되어 잠재적인 사이드 이펙트나 버그 조기에 잡아냄.
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
--- coin-tracker/src/routes/Coins.tsx
+++ coin-tracker/src/routes/Coins.tsx
@@ -1,12 +1,16 @@
+import { response } from "express";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { styled } from "styled-components"
 
 const Container = styled.div`
     padding: 10px;
+    max-width: 480px;
+    margin: 0 auto;
 `;
 
 const Header = styled.header`
-    height: 10vh;
+    height: 15vh;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -22,7 +26,8 @@ const Coin = styled.li`
     a {
         padding: 20px;
         transition: color 0.2s ease-in;
-        display: block;
+        display: flex;
+        align-items: center;
     }
     &:hover {
         a {
@@ -36,46 +41,52 @@ const Title = styled.h1`
     color: ${props => props.theme.accentColor};
 `;
 
-const coins = [
-    {
-        id: "btc-bitcoin",
-        name: "Bitcoin",
-        symbol: "BTC",
-        rank: 1,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-    },
-    {
-        id: "eth-ethereum",
-        name: "Ethereum",
-        symbol: "ETH",
-        rank: 2,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-    },
-    {
-        id: "hex-hex",
-        name: "HEX",
-        symbol: "HEX",
-        rank: 3,
-        is_new: false,
-        is_active: true,
-        type: "token",
-    },
-];
+const Loader = styled.div`
+    text-align: center;
+`;
+
+const Img = styled.img`
+    width: 30px;
+    height: 30px;
+    margin-right: 10px;
+`
+
+interface ICoin {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+}
 
 export default function Coins() {
+    const [coins, setCoins] = useState<ICoin[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/coins")
+            .then(response => response.json())
+            .then(data => {
+                setLoading(false);
+                setCoins(data.slice(0, 100));
+            });
+    }, [])
+
     return (
         <Container>
             <Header>
                 <Title>Coins</Title>
             </Header>
+            {loading && <Loader>Loading...</Loader>}
             <CoinList>
                 {coins.map(coin => (
                     <Coin key={coin.id}>
-                        <Link to={coin.id}>
+                        <Link to={coin.id} state={coin} >
+                            <Img
+                                src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
+                            />
                             {coin.name} &rarr;
                         </Link>
                     </Coin>
