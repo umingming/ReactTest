776603fb085b1012943182558269d10a02851ce1
umingming <u_0112@naver.com>
2024-09-13 23:25:57 +0900

    feat: Validate input value

--- to-do/src/ToDoList.tsx
+++ to-do/src/ToDoList.tsx
@@ -23,3 +23,46 @@ import { useForm } from "react-hook-form";
 //         </div>
 //     );
 // }
+
+export function ToDoList() {
+    /*
+        watch를 사용해 값을 추적할 수 있다.
+    */
+    const { register, watch, handleSubmit, formState } = useForm();
+    // console.log(watch())
+    console.log("errors:", formState.errors)
+
+    const onValid = (data: any) => {
+        console.log("valid:", data);
+    }
+
+    // 실패한 인풋으로 포커싱 이동됨.
+    const onInvalid = (data: any) => {
+        console.log("invalid:", data);
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit(onValid, onInvalid)}>
+                <input 
+                    {...register("toDo1", { 
+                        required: true, 
+                        minLength: {
+                            value: 5,
+                            message: "test"
+                        }
+                    })} 
+                    placeholder="Write a to do" 
+                />
+                {/* required value에 문자열을 넣으면 그게 메시지다. */}
+                <input 
+                    {...register("toDo2", { 
+                        required: "Required" 
+                    })} 
+                    placeholder="Write a to do"
+                />
+                <button>Add</button>
+            </form>
+        </div>
+    );
+}
