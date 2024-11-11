8a682f77a2809934f27df13a163e8707e9a796b6
umingming <u_0112@naver.com>
2024-11-11 23:21:40 +0900

    feat: Update parent context

--- modal-context/src/App.tsx
+++ modal-context/src/App.tsx
@@ -1,11 +1,18 @@
-import { createContext, useContext } from "react";
+import { createContext, useState } from "react";
 
-const ThemeContext = createContext("light");
+const ThemeContext = createContext({
+  theme: "light",
+  toggleTheme: () => {},
+});
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+  const toggleTheme = () =>
+    setTheme((theme) => (theme === "dark" ? "light" : "dark"));
+
   return (
     <div className="App">
-      <ThemeContext.Provider value="dark">
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
         <Toolbar />
       </ThemeContext.Provider>
     </div>
@@ -21,8 +28,13 @@ function Toolbar() {
 }
 
 function ThemedButton() {
-  const theme = useContext(ThemeContext);
-  return <button>{theme}</button>;
+  return (
+    <ThemeContext.Consumer>
+      {({ theme, toggleTheme }) => (
+        <button onClick={toggleTheme}>{theme}</button>
+      )}
+    </ThemeContext.Consumer>
+  );
 }
 
 export default App;
