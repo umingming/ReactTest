863e8246b9d884410a60835c1db2723696f1b94c
umingming <u_0112@naver.com>
2024-11-13 22:55:52 +0900

    feat: Define board component

--- react-doc/src/index.tsx
+++ react-doc/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Board from "./components/TicTacToe";
 
 // const root = ReactDOM.createRoot(
 //   document.getElementById('root') as HTMLElement
@@ -17,18 +18,20 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  // 매초마다 호출하나, 변경된 부분만 업데이트함.
-  root.render(element);
-}
+root.render(<Board />);
 
-setInterval(tick, 1000);
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   // 매초마다 호출하나, 변경된 부분만 업데이트함.
+//   root.render(element);
+// }
+
+// setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
