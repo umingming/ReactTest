56f2248c9b2b1cc890b33c6a9340efede638a55f
umingming <u_0112@naver.com>
2024-12-10 22:58:06 +0900

    Init test project

---
+++ my-test/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return <div className="App flex justify-center items-center">
+    <div className="h-40 w-40 bg-slate-500"></div>
+  </div>;
+}
+
+export default App;
