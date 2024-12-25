a8a0c20e3c96cb677e068dbbfd2bff410938b522
umingming <u_0112@naver.com>
2024-12-25 16:14:49 +0900

    test: Adjust fontsize

--- my-test/src/App.tsx
+++ my-test/src/App.tsx
@@ -2,18 +2,27 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const {innerHeight, outerHeight, devicePixelRatio, screen, visualViewport} = window;
-  return <div className="App h-screen flex justify-center flex-col items-center">
-    <div className="h-40 w-40 bg-slate-300">
-      Height: {innerHeight} {outerHeight}
-      <br />
-      Ratio: {devicePixelRatio}
-      <br />
-      Screen: {screen.height}
-      <br />
-      Scale: {visualViewport?.height} {visualViewport?.scale}
-    </div>
-  </div>;
+    const {
+        innerHeight,
+        outerHeight,
+        devicePixelRatio,
+        screen,
+        visualViewport,
+    } = window;
+    return (
+        <div className="App flex h-screen flex-col items-center justify-center">
+            <div className="pc:bg-purple-600 tablet:bg-teal-400 mobile:bg-orange-500 h-200 w-200 bg-slate-300 text-5xl">
+                Height: {innerHeight} {outerHeight}
+                <br />
+                Ratio: {devicePixelRatio}
+                <br />
+                Screen: {screen.height}
+                <br />
+                Scale: {visualViewport?.height} {visualViewport?.scale}
+                <p className="text-[48px]">TEST</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- my-test/tailwind.config.js
+++ my-test/tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
     content: ["./src/**/*.{html,js,tsx,ts}"],
     theme: {
+        screens: {
+            mobile: {
+                max: "799px",
+            },
+            tablet: {
+                raw: "(min-width: 601px) and (max-width: 1334px) and (orientation: portrait)",
+            },
+            pc: {
+                raw: "(min-width: 800px) and (orientation: landscape), (min-width: 1335px)",
+            },
+        },
         extend: {},
     },
     plugins: [],
