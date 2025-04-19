3ccdc8ded788652472a8866ce00e282f8a7fafa6
umingming <u_0112@naver.com>
2025-04-19 23:53:29 +0900

    refactor: 정리

--- dnd/src/dnd/ListBox.js
+++ dnd/src/dnd/ListBox.js
@@ -11,21 +11,26 @@ const ListItem = ({ text, index, moveListItem }) => {
     }),
   });
 
-  // useDrop - the list item is also a drop area
-  const [_, dropRef] = useDrop({
-    accept: "item",
+  const [, drop] = useDrop({
+    accept: ItemType,
     hover: (item, monitor) => {
+      if (!ref.current) return;
+
       const dragIndex = item.index;
       const hoverIndex = index;
-      const hoverBoundingRect = ref.current?.getBoundingClientRect();
-      const hoverMiddleY =
-        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-      const hoverActualY = monitor.getClientOffset().y - hoverBoundingRect.top;
+      if (dragIndex === hoverIndex) return;
 
-      // if dragging down, continue only when hover is smaller than middle Y
-      if (dragIndex < hoverIndex && hoverActualY < hoverMiddleY) return;
-      // if dragging up, continue only when hover is bigger than middle Y
-      if (dragIndex > hoverIndex && hoverActualY > hoverMiddleY) return;
+      const { top, bottom } = ref.current.getBoundingClientRect();
+      const hoverMiddleY = (bottom - top) / 2;
+      const clientOffset = monitor.getClientOffset();
+      const hoverClientY = clientOffset.y - top;
+
+      if (
+        (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) ||
+        (dragIndex > hoverIndex && hoverClientY > hoverMiddleY)
+      ) {
+        return;
+      }
 
       moveListItem(dragIndex, hoverIndex);
       item.index = hoverIndex;
